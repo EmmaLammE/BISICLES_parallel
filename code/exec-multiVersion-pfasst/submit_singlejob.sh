bf574e2988988bc3dc9050c17b87143befebec4c
emmalamme <liuwj@stanford.edu>
2024-06-09 19:45:09 -0700

    updated some sbatch files

--- code/exec-multiVersion-pfasst/submit_singlejob.sh
+++ code/exec-multiVersion-pfasst/submit_singlejob.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=t1s1
+#SBATCH --job-name=t8s32
 #SBATCH --qos=regular
-#SBATCH --time=24:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=40G
+#SBATCH --time=03:00:00
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=8G
 #SBATCH --constraint=cpu
-#SBATCH -o ./sbatch_outputs/out_pf_amr2_nt1200_tfin20_t1s1.%j.out
-#SBATCH -e ./sbatch_outputs/err_pf_amr2_nt1200_tfin20_t1s1.%j.err
+#SBATCH -o ./sbatch_outputs/out_pf_amr2_nt256_tfin20_t8s32.%j.out
+#SBATCH -e ./sbatch_outputs/err_pf_amr2_nt256_tfin20_t8s32.%j.err
 
 
-srun ./driver2d.Linux.64.CC.ftn.DEBUG.OPT.MPI.ex inputs.test
+srun -n 256 ./driver2d.Linux.64.CC.ftn.DEBUG.OPT.MPI.ex ./inputs_multiruns/inputs_nt256_tfin20_t8s32.test
